57188b55bfe058b8030e9e375a5f815a7ef1eaa3
ZinZinNguyen <71427292+ZinZinNguyen@users.noreply.github.com>
2021-01-16 16:42:41 +0700

    fisrt commit
    hihi

---
+++ app/src/main/java/com/example/drumapp/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.drumapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+}
