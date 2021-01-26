9dd2c01de8ac2a7b5d9b7e86111fbd9df30590b4
Khánh Trần Duy <18520075@gm.uit.edu.vn>
2021-01-26 21:23:58 +0700

    Change: Settings layout

--- app/src/main/java/com/example/drumapp/SettingsActivity.java
+++ app/src/main/java/com/example/drumapp/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.drumapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,29 @@ import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    Spinner[] spinners;
+    Button[] plays;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        spinners = new Spinner[6];
+        plays = new Button[6];
+        for(int i = 0; i < 6; i ++)
+        {
+            String name = "";
+            switch (i)
+            {
+                case 0: name = "hh"; break;
+                case 1: name = "ki"; break;
+                case 2: name = "sn"; break;
+                case 3: name = "pi"; break;
+                case 4: name = "ba"; break;
+                case 5: name = "bo"; break;
+            }
+            String sname = name + "spinner";
+            String pname = name + "play";
+            spinners[i] = (Spinner) findViewById(getResources().getIdentifier(sname, "id", getPackageName()));
+            plays[i] = (Button) findViewById(getResources().getIdentifier(pname, "id", getPackageName()));
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
     }
